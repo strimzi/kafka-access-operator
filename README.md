f41cd6d5a1b97438a3212a3a1ab3dac6ce70d5cd
Katherine Stanley <11195226+katheris@users.noreply.github.com>
2022-01-11 13:50:21 +0000

    feat: Add boilerplate files for operator
    Signed-off-by: Katherine Stanley <11195226+katheris@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 [![License](https://img.shields.io/badge/license-Apache--2.0-blue.svg)](http://www.apache.org/licenses/LICENSE-2.0)
 [![Twitter Follow](https://img.shields.io/twitter/follow/strimziio.svg?style=social&label=Follow&style=for-the-badge)](https://twitter.com/strimziio)
 
-# Strimzi Template repository
+# Kafka Access Operator
 
-This repository is currently empty
+Kubernetes operator that collects connection details for an Apache Kafka cluster that is managed by the Strimzi cluster 
+operator into a Kubernetes secret. (See [proposal #33](https://github.com/strimzi/proposals/blob/main/033-service-binding.md) for more details.)
+
+This operator uses the [Java Operator SDK](https://github.com/java-operator-sdk/java-operator-sdk).
---
+++ src/main/java/io/strimzi/kafka/access/KafkaAccessOperator.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright Strimzi authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+package io.strimzi.kafka.access;
+
+import io.fabric8.kubernetes.client.Config;
+import io.fabric8.kubernetes.client.ConfigBuilder;
+import io.fabric8.kubernetes.client.DefaultKubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.javaoperatorsdk.operator.Operator;
+import io.javaoperatorsdk.operator.config.runtime.DefaultConfigurationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class KafkaAccessOperator {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaAccessOperator.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("Kafka Access operator starting");
+        Config config = new ConfigBuilder().withNamespace(null).build();
+        KubernetesClient client = new DefaultKubernetesClient(config);
+        Operator operator = new Operator(client, DefaultConfigurationService.instance());
+        operator.register(new KafkaAccessReconciler(client));
+        operator.installShutdownHook();
+        operator.start();
+        LOGGER.info("Kafka Access operator started");
+    }
+}
