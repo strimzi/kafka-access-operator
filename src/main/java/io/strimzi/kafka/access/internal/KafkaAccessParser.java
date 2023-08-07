fdf9dd628dfba1548df219ff49bcfa3bcb0b97b0
Kate Stanley <11195226+katheris@users.noreply.github.com>
2023-08-07 10:52:53 +0100

    fix: Add event source for Strimzi user secrets (#27)
    Signed-off-by: Katherine Stanley <11195226+katheris@users.noreply.github.com>

--- src/main/java/io/strimzi/kafka/access/internal/KafkaAccessParser.java
+++ src/main/java/io/strimzi/kafka/access/internal/KafkaAccessParser.java
@@ -44,9 +44,19 @@ public class KafkaAccessParser {
     public static final String INSTANCE_LABEL_KEY = "app.kubernetes.io/instance";
 
     /**
+     *  The constant for Strimzi cluster label
+     */
+    public static final String STRIMZI_CLUSTER_LABEL_KEY = "strimzi.io/cluster";
+
+    /**
      * The constant for Strimzi cluster operator label
      */
     public static final String STRIMZI_CLUSTER_LABEL_VALUE = "strimzi-cluster-operator";
+
+    /**
+     * The constant for Strimzi user operator label
+     */
+    public static final String STRIMZI_USER_LABEL_VALUE = "strimzi-user-operator";
 
     /**
      * The constant for Strimzi access operator label
@@ -63,7 +73,7 @@ public class KafkaAccessParser {
      *
      * @return                   Set of ResourceIDs for the KafkaAccess objects that reference the Kafka resource
      */
-    public static Set<ResourceID> getKafkaAccessSetForKafka(final Stream<KafkaAccess> kafkaAccessList, final Kafka kafka) {
+    public static Set<ResourceID> kafkaSecondaryToPrimaryMapper(final Stream<KafkaAccess> kafkaAccessList, final Kafka kafka) {
         final Optional<String> kafkaName = Optional.ofNullable(kafka.getMetadata()).map(ObjectMeta::getName);
         final Optional<String> kafkaNamespace = Optional.ofNullable(kafka.getMetadata()).map(ObjectMeta::getNamespace);
         if (kafkaName.isEmpty() || kafkaNamespace.isEmpty()) {
@@ -88,7 +98,7 @@ public class KafkaAccessParser {
      *
      * @return                   Set of ResourceIDs for the KafkaAccess objects that reference the KafkaUser resource
      */
-    public static Set<ResourceID> getKafkaAccessSetForKafkaUser(final Stream<KafkaAccess> kafkaAccessList, final KafkaUser kafkaUser) {
+    public static Set<ResourceID> kafkaUserSecondaryToPrimaryMapper(final Stream<KafkaAccess> kafkaAccessList, final KafkaUser kafkaUser) {
         final Optional<String> kafkaUserName = Optional.ofNullable(kafkaUser.getMetadata()).map(ObjectMeta::getName);
         final Optional<String> kafkaUserNamespace = Optional.ofNullable(kafkaUser.getMetadata()).map(ObjectMeta::getNamespace);
         if (kafkaUserName.isEmpty() || kafkaUserNamespace.isEmpty()) {
@@ -131,7 +141,7 @@ public class KafkaAccessParser {
      *
      * @return                   Set of ResourceIDs for the KafkaAccess objects that reference the Kafka resource
      */
-    public static Set<ResourceID> getKafkaAccessResourceIDsForSecret(final Stream<KafkaAccess> kafkaAccessList, final Secret secret) {
+    public static Set<ResourceID> secretSecondaryToPrimaryMapper(final Stream<KafkaAccess> kafkaAccessList, final Secret secret) {
         final Set<ResourceID> resourceIDS = new HashSet<>();
 
         final Optional<String> secretNamespace = Optional.ofNullable(secret.getMetadata())
@@ -146,28 +156,38 @@ public class KafkaAccessParser {
                 .map(ObjectMeta::getLabels)
                 .orElse(new HashMap<>());
 
-        if (KAFKA_ACCESS_LABEL_VALUE.equals(labels.get(MANAGED_BY_LABEL_KEY))) {
-            Optional.ofNullable(secret.getMetadata())
-                .map(ObjectMeta::getOwnerReferences)
-                .orElse(Collections.emptyList())
-                .stream()
-                .filter(ownerReference -> KafkaAccess.KIND.equals(ownerReference.getKind()))
-                .findFirst()
-                .map(OwnerReference::getName)
-                .ifPresent(s -> resourceIDS.add(new ResourceID(s, secretNamespace.get())));
+        final String managedByLabel = labels.get(MANAGED_BY_LABEL_KEY);
+        if (managedByLabel == null) {
+            LOGGER.error("Secret missing managed-by label, returning empty list.");
+            return resourceIDS;
         }
-
-        if (STRIMZI_CLUSTER_LABEL_VALUE.equals(labels.get(MANAGED_BY_LABEL_KEY))) {
-            Optional.ofNullable(labels.get(INSTANCE_LABEL_KEY))
-                .ifPresent(clusterName -> {
-                    final Kafka kafka = new KafkaBuilder()
-                            .withNewMetadata()
-                            .withName(clusterName)
-                            .withNamespace(secretNamespace.get())
-                            .endMetadata()
-                            .build();
-                    resourceIDS.addAll(KafkaAccessParser.getKafkaAccessSetForKafka(kafkaAccessList, kafka));
-                });
+        if (KAFKA_ACCESS_LABEL_VALUE.equals(managedByLabel)) {
+            Optional.ofNullable(secret.getMetadata())
+                    .map(ObjectMeta::getOwnerReferences)
+                    .orElse(Collections.emptyList())
+                    .stream()
+                    .filter(ownerReference -> KafkaAccess.KIND.equals(ownerReference.getKind()))
+                    .findFirst()
+                    .map(OwnerReference::getName)
+                    .ifPresent(s -> resourceIDS.add(new ResourceID(s, secretNamespace.get())));
+        } else {
+            final String clusterName = switch (managedByLabel) {
+                case STRIMZI_CLUSTER_LABEL_VALUE -> labels.get(INSTANCE_LABEL_KEY);
+                case STRIMZI_USER_LABEL_VALUE -> labels.get(STRIMZI_CLUSTER_LABEL_KEY);
+                default -> {
+                    LOGGER.error("Secret managed by unknown resource {}.", managedByLabel);
+                    yield null;
+                }
+            };
+            if (clusterName != null) {
+                final Kafka kafka = new KafkaBuilder()
+                        .withNewMetadata()
+                        .withName(clusterName)
+                        .withNamespace(secretNamespace.get())
+                        .endMetadata()
+                        .build();
+                resourceIDS.addAll(KafkaAccessParser.kafkaSecondaryToPrimaryMapper(kafkaAccessList, kafka));
+            }
         }
 
         return resourceIDS;
@@ -180,7 +200,7 @@ public class KafkaAccessParser {
      *
      * @return               Set of ResourceIDs containing the KafkaUser that is referenced by the KafkaAccess
      */
-    public static Set<ResourceID> getKafkaUserForKafkaAccess(final KafkaAccess kafkaAccess) {
+    public static Set<ResourceID> kafkaUserPrimaryToSecondaryMapper(final KafkaAccess kafkaAccess) {
         final Set<ResourceID> resourceIDS = new HashSet<>();
         Optional.ofNullable(kafkaAccess.getSpec())
                 .map(KafkaAccessSpec::getUser)
