39db941c20c1acd53cc845498f6ea14e4ab543df
Luca Burgazzoli <lburgazzoli@gmail.com>
2023-10-23 14:12:56 +0200

    Better status condition management #41
    Signed-off-by: Luca Burgazzoli <lburgazzoli@gmail.com>

--- src/main/java/io/strimzi/kafka/access/model/KafkaAccessStatus.java
+++ src/main/java/io/strimzi/kafka/access/model/KafkaAccessStatus.java
@@ -7,12 +7,8 @@ package io.strimzi.kafka.access.model;
 import io.javaoperatorsdk.operator.api.ObservedGenerationAwareStatus;
 import io.strimzi.api.kafka.model.status.Condition;
 
-import java.time.ZoneOffset;
-import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 /**
  * The status model of the KafkaAccess resource
@@ -20,7 +16,7 @@ import java.util.Optional;
 public class KafkaAccessStatus extends ObservedGenerationAwareStatus {
 
     private BindingStatus binding;
-    private List<Condition> conditions = new ArrayList<>();
+    private final List<Condition> conditions = new ArrayList<>();
 
     /**
      * Gets the BindingStatus instance
@@ -55,7 +51,10 @@ public class KafkaAccessStatus extends ObservedGenerationAwareStatus {
      * @param ready Whether the resource is ready
      */
     public void setReadyCondition(final boolean ready) {
-        setReadyCondition(ready, null, null);
+        StatusUtils.setCondition(
+            this.conditions,
+            StatusUtils.buildReadyCondition(ready, null, null)
+        );
     }
 
     /**
@@ -66,15 +65,9 @@ public class KafkaAccessStatus extends ObservedGenerationAwareStatus {
      * @param reason The reason for the status condition
      */
     public void setReadyCondition(final boolean ready, final String message, final String reason) {
-        final Condition condition = new Condition();
-        condition.setType("Ready");
-        condition.setStatus(ready ? "True" : "False");
-        condition.setLastTransitionTime(ZonedDateTime.now(ZoneOffset.UTC).format(DateTimeFormatter.ISO_INSTANT));
-        Optional.ofNullable(message)
-                .ifPresent(condition::setMessage);
-        Optional.ofNullable(reason)
-                .ifPresent(condition::setReason);
-        this.conditions = List.of(condition);
+        StatusUtils.setCondition(
+            this.conditions,
+            StatusUtils.buildReadyCondition(ready, reason, message)
+        );
     }
-
 }
