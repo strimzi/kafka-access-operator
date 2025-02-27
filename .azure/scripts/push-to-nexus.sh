33a2493f7f06830b858f9d5c792fc596e4fa0fa4
Jakub Scholz <www@scholzj.com>
2025-02-27 20:11:51 +0100

    Add publishing of the API artifact to Maven repositories (#77)
    Signed-off-by: Jakub Scholz <www@scholzj.com>

---
+++ .azure/scripts/push-to-nexus.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "Build reason: ${BUILD_REASON}"
+echo "Source branch: ${BRANCH}"
+
+function cleanup() {
+  rm -rf signing.gpg
+  gpg --delete-keys
+  gpg --delete-secret-keys
+}
+
+# Run the cleanup on failure / exit
+trap cleanup EXIT
+
+export GPG_TTY=$(tty)
+echo $GPG_SIGNING_KEY | base64 -d > signing.gpg
+gpg --batch --import signing.gpg
+
+GPG_EXECUTABLE=gpg mvn $MVN_ARGS -DskipTests -s ./.azure/scripts/settings.xml -pl ./,api -P ossrh deploy
+
+cleanup
