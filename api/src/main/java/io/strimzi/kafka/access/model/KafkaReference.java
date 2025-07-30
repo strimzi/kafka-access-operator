bc9fa29e312b91e1abd8f0c87c2ee6739d5f07cd
MichaelMorris <michael.morris@est.tech>
2025-07-30 17:46:13 +0100

    Add Ready column to KafkaAccess and add KafkaAccess to strimzi category
    Signed-off-by: MichaelMorris <michael.morris@est.tech>

--- api/src/main/java/io/strimzi/kafka/access/model/KafkaReference.java
+++ api/src/main/java/io/strimzi/kafka/access/model/KafkaReference.java
@@ -18,10 +18,8 @@ import io.sundr.builder.annotations.Buildable;
 public class KafkaReference {
 
     @Required
-    @io.fabric8.crd.generator.annotation.PrinterColumn(name = "Cluster")
     private String name;
     private String namespace;
-    @io.fabric8.crd.generator.annotation.PrinterColumn(name = "Listener")
     private String listener;
 
     /**
