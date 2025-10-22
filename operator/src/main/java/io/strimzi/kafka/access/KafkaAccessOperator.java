5fc02158e2718f075abb0d0de553478f717870c0
OwenCorrigan76 <owencorrigan76@gmail.com>
2025-10-22 13:29:31 +0100

    Remove withUSESSAToPatch
    Signed-off-by: OwenCorrigan76 <owencorrigan76@gmail.com>

--- operator/src/main/java/io/strimzi/kafka/access/KafkaAccessOperator.java
+++ operator/src/main/java/io/strimzi/kafka/access/KafkaAccessOperator.java
@@ -26,8 +26,7 @@ public class KafkaAccessOperator {
      */
     public static void main(final String[] args) {
         LOGGER.info("Kafka Access operator starting");
-        final Operator operator = new Operator(overrider -> overrider
-                .withUseSSAToPatchPrimaryResource(false));
+        final Operator operator = new Operator();
         operator.register(new KafkaAccessReconciler(operator.getKubernetesClient()));
         operator.start();
         Server server = new Server(HEALTH_CHECK_PORT);
